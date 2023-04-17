b27f02f4681222631c5aecf0c8adf8575db42409
byoso <emailvinz@gmail.com>
2023-04-17 16:12:43 +0200

    fixed icon bug in the starter ploper

--- flask_fd/plop/ploppers.py
+++ flask_fd/plop/ploppers.py
@@ -35,6 +35,8 @@ def plop_starter():
     if os.path.exists(os.path.join(cwd, "flask_fd_app")):
         print("Aborted: flask_fd_app already exists in the current directory")
     else:
+        # icon
+        _plop_icon(cwd)
         shutil.copytree(folder, os.path.join(cwd, "flask_fd_app"))
         set_executable(os.path.join(cwd, 'flask_fd_app/flask_for_desktop.py'))
         print("Flask-fd starter kit ploped !")
